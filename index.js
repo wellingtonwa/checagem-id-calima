a251d2b66a82dee042bd780cd290f6fd2ee0bea0
Wellington Wagner <wellingtonwa@gmail.com>
2020-04-03 10:27:13 -0300

    melhorias na organização do código

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const ioUtils = require('./utils/ioUtils');
 const REGEX_PADRAO_NOME = { '.tsx': /^[A-Z]\w*/, '.ts': /^[a-z]\w*/ };
 const REGEX_ROUTES = /.*(routes).ts$/i;
 const REGEX_MENU = /.*(menu).ts$/i;
+const REGEX_INPUTS = /<(InputGroupInline|CustomInput|\w*IS)(.|\r\n)*?\/>/gm;
+const REGEX_PATH_MENU = /'([\w0-9]*)'(?!;)/gm;
+const REGEX_CAMINHO_MENU = /(?<=to: '\/[a-z]{3}\/)(.*)?(?=')/gm;
 
 const options_tsx = { 
     initial_path: 'D:/sistemas/calima-react/react/src/views/pages/',
@@ -47,16 +50,13 @@ const varrerDiretorios = (caminho, extensao_arquivo, arquivos_encontrados) => {
 const run = (opts) => {
     varrerDiretorios(opts.initial_path, opts.extname, opts.arquivos_encontrados);
 }
-(async () => {
-    const regex1 = /<(InputGroupInline|CustomInput|\w*IS)(.|\r\n)*?\/>/gm;
-    const REGEX_PATH_MENU = /'([\w0-9]*)'(?!;)/gm;
-    const REGEX_CAMINHO_MENU = /(?<=to: '\/[a-z]{3}\/)(.*)?(?=')/gm;
-    run(options_tsx);
-    run(options_ts);
+
+ // Método para pesquisar se os inputs tem o atributo id informado
+const checkIds = async (options_tsx) => {
     for(let arquivo of options_tsx.arquivos_encontrados) {
         let conteudo = await ioUtils.getFileContent({filePath: arquivo});
         let array1;
-        while ((array1 = regex1.exec(conteudo)) !== null) {
+        while ((array1 = REGEX_INPUTS.exec(conteudo)) !== null) {
             const input = array1[0].replace(/(\n|\r\n| {2,})/g, ' ');
             if (input.indexOf('id=') < 0) {
                 console.log(`ID não encontrado ${input}. Arquivo: ${arquivo}`);
@@ -64,14 +64,11 @@ const run = (opts) => {
         }
         
     }
+}
 
-    run(options_menu_ts);
-
-    // console.log(options_menu_ts.arquivos_encontrados)
-    // console.log(path.basename('D:\\sistemas\\calima-react\\react\\src\\models\\dtos\\mlf\\mfMenu.ts').match(REGEX_ROUTES))
-     let rotas = options_menu_ts.arquivos_encontrados.filter(it => path.basename(it).match(REGEX_ROUTES));
-     let menu = options_menu_ts.arquivos_encontrados.filter(it => path.basename(it).match(REGEX_MENU));
-    // let rotas = ['D:\\sistemas\\calima-react\\react\\src\\views\\pages\\mlf\\mlfRoutes.ts']:
+const checkPagesMenu = async (options_menu_ts) => {
+    let rotas = options_menu_ts.arquivos_encontrados.filter(it => path.basename(it).match(REGEX_ROUTES));
+    let menu = options_menu_ts.arquivos_encontrados.filter(it => path.basename(it).match(REGEX_MENU));
     let arquivos_no_menu = []
     for(let arquivo of rotas) {
         let conteudo = await ioUtils.getFileContent({filePath: arquivo});
@@ -82,8 +79,6 @@ const run = (opts) => {
         }
     }
 
-    arquivos_no_menu.sort().forEach(it => console.log("rota: ", it));
-
     for(let arquivo of menu) {
         let conteudo = await ioUtils.getFileContent({filePath: arquivo})
         let array1;
@@ -93,13 +88,23 @@ const run = (opts) => {
         }
     }
 
-    // console.log(arquivos_no_menu)
-
     for(let arquivo of options_tsx.arquivos_encontrados) {
         let aux_arquivo_menu = path.basename(arquivo, '.tsx').toLowerCase();
         if (!arquivos_no_menu.find(it => it == aux_arquivo_menu)) {
             console.log(`A tela ${aux_arquivo_menu} não está no menu!`);
         }
     }
+}
+
+(async () => {
+    
+    run(options_tsx);
+    run(options_ts);
+    run(options_menu_ts);
+    
+    // Verificando se os inputs tem o atributo id informado
+    checkIds(options_tsx);
+    // Verificando se as paginas do projeto estão no menu
+    checkPagesMenu(options_menu_ts);
 
 })()
